29d334db6bd01bfc16d7fc13b2dcfee7c442726a
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-04-13 13:44:31 -0400

    [RESPONSAPP] finish the alpha 1

--- servidor/js/script.js
+++ servidor/js/script.js
@@ -1,10 +1,14 @@
-jQuery(function() {
+jQuery(function(){
     config=new(function(){
         this.server='http://bridge/lib/ajax.php'
         this.sender=function(task){
             jQuery.post(config.server,{'task':task},function(data){
                 console.log(data)
             })
+        }
+        this.vdelta=function(x,y){
+            jQuery.post(config.server,{'task':'vdelta','x':x,'y':y},
+                function(data){console.log(data)})
         }
     })()
 
@@ -15,6 +19,16 @@ jQuery(function() {
     $('#f_on').click(function(){
         console.log('connect the video')
         $(this).addClass('active').siblings().removeClass('active')
+    })
+    $('#f_video').click(function(){
+        console.log('video broadcast')
+        $(this).addClass('active').siblings().removeClass('active')
+        $('#r_camera').removeClass().addClass('video');
+    })
+    $('#f_photo').click(function(){
+        console.log('microscopic camera')
+        $(this).addClass('active').siblings().removeClass('active')
+        $('#r_camera').removeClass().addClass('photo');
     })
     $('#f_shot').click(function(){
         console.log('taking a picture')
@@ -36,7 +50,7 @@ jQuery(function() {
         console.log('rotate to right ...')
         config.sender('right')
     })
-    $('#f_vup').click(function(){
+    /*$('#f_vup').click(function(){
         console.log('up for the camera')
         config.sender('vup')
     })
@@ -51,6 +65,13 @@ jQuery(function() {
     $('#f_vright').click(function(){
         console.log('turn right for the camera')
         config.sender('vright')
-    })
+    })*/
+
+    joystick=new VirtualJoystick({
+        container:document.getElementById('r_camera'),
+        mouseSupport:true
+    });
+    setInterval(function(){config.vdelta(joystick.deltaX(),joystick.deltaY())},
+        1/2*1000);
 })
 
